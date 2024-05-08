415405021fc6fa094a3e1067c9511bdb458aa281
BTheDream <bferrell2123@gmail.com>
2024-05-07 23:59:19 -0400

    Add solutions for days 0-3

--- day003_conditionalStatements/day003_conditionalStatements.js
+++ day003_conditionalStatements/day003_conditionalStatements.js
@@ -20,8 +20,19 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
-
 function main() {
     const N = parseInt(readLine().trim(), 10);
+    // Write your code here
+
+    if (N % 2 !== 0) {
+        console.log('Weird');
+    } else {
+        if (N >= 2 && N <= 5) {
+            console.log('Not Weird');
+        } else if (N >= 6 && N <= 20) {
+            console.log('Weird');
+        } else {
+            console.log('Not Weird');
+        }
+    }
 }
